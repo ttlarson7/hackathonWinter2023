f6167a2f09aa1a3e6d1279668ffd60a05860f021
Taz Larson <tazlarson1@gmail.com>
2024-03-01 18:53:56 -0800

    more setup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import 'daisyui/dist/full.css';
+import { Route, Routes, BrowserRouter, useNavigate } from "react-router-dom";
+import Landing from './routes/Landing'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-        
-    </>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
   )
 }
 
