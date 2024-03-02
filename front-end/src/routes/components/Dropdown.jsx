11e4b5c80bbe0edaa6694706a295fadf6ddb5d74
Taz Larson <tazlarson1@gmail.com>
2024-03-02 15:58:02 -0800

    testing backend

--- front-end/src/routes/components/Dropdown.jsx
+++ front-end/src/routes/components/Dropdown.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { GlobalStateContext } from "../../App";
-
+import {SignInButton, SignedIn, SignedOut, UserButton, useAuth} from "@clerk/clerk-react"
 export default function Dropdown() {
     const {globalRace, setGlobalRace, 
         globalClass, setGlobalClass,
@@ -8,12 +8,33 @@ export default function Dropdown() {
         globalBackground, setGlobalBackground,
         globalDescription, setGlobalDescription
     } = React.useContext(GlobalStateContext);
+    const [characters, setCharacters] = React.useContext(GlobalStateContext);
+    const [name, setName] = useState("ASD");
+    const [cl, setCl] = useState("asd");
+    const [race, setRace] = useState("asf");
+    const [subRace, setSubRace] = useState("asdf");
+    const [background, setBackground] = useState("asdf");
+    const [alignment, setAlignment] = useState("adf");
+    const [languages, setLanguages] = useState("asdf");
+    const [description, setDescription] = useState("asdf");
+    const [stats, setStats] = useState([1]);
+    const [abilities, setAbilities] = useState(['as']);
 
+    //user inputs: name, class, race, subrace, background, alignment, description, 
+    //what we need to query: race, subrace, class, background, 
+    
+    const getRaceAbilities = async () => {
+        const baseURL = 'https://www.dnd5eapi.co/api/races/'
+        const response = await fetch(baseURL + globalRace.toLowerCase());
+        const data = await response.json();
 
+    }
+    
+    
     const backgrounds = ["Noble", "Outlander", "Acolyte", "Folk Hero"];
-    const alignments = ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Evil"];
-    const classes = ["Warrior", "Sorcerer", "Rogue", "Cleric", "Wizard"];
-    const races = ["Goblin", "Human", "Dwarf", "Gnome", "Elf", "Half Elf", "Halfling", "Half Orc", "Tiefling"];
+    const alignments = ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Evil", "Neutral Evil", "Chaotic Evil", "Lawful Neutral", "True Neutral", "Chaotic Neutral"];
+    const classes = ["Fighter", "Sorcerer", "Rogue", "Cleric", "Wizard", "Bard", "Druid", "Monk", "Barbarian", "Paladin", "Ranger"];
+    const races = ["Human", "Dwarf", "Gnome", "Elf", "Half-Elf", "Halfling", "Half-Orc", "Tiefling", "Dragonborn"];
 
     const handleSelectionChange = (e, setState) => {
         setState(e.target.value);
@@ -26,20 +47,41 @@ export default function Dropdown() {
     const handleSubmit = (e) => {
         e.preventDefault(); 
         
-        //testing
-        console.log("Selected Race:", globalRace);
-        console.log("Selected Class:", globalClass);
-        console.log("Selected Alignment:", globalAlignment);
-        console.log("Selected Background:", globalBackground);
-        console.log("setGlobalDescription:", globalDescription);
+       
+    }
+    const { getToken } = useAuth();
+    const createCharacter = async () => {
+        const data = {
+            
+            userId: "",
+            name: name,
+            class: cl,
+            race: race,
+            subRace: subRace,
+            background: background,
+            alignment: alignment,
+            languages: languages,
+            description: description,
+            stats: stats,
+            abilities: abilities
+        }
+        setCharacters([...characters, data]);
+        const response = await fetch('/api/characters', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${await getToken()}`
+            },
+            body: JSON.stringify(data)
+          })
     }
 
     return (
-        <div id="category-dropdown" className="p-4 bg-white shadow-md rounded-md">
+        <div id="category-dropdown" className="p-4 bg-white shadow-md rounded-md ml-10">
             <header className="text-center text-lg font-semibold text-gray-800 mb-4"> 
-                Character Select
+                Character Create
             </header>
-
+            <input type="text" placeholder="Character Name" className="input input-bordered input-primary w-full max-w-xs mb-5" />
             <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                 <select 
                     className="p-2 border border-primary rounded-md shadow-sm focus:outline-none focus:border-blue-200"
@@ -96,7 +138,7 @@ export default function Dropdown() {
                     value={globalDescription}
                     onChange={handleDescriptionChange}
                 />                
-                <button className="p-2 bg-quad text-white rounded-md hover:bg-tertiary hover:text-black" type="submit">Submit</button>
+                <button className="p-2 bg-quad text-white rounded-md hover:bg-tertiary hover:text-black" type="submit" onClick={createCharacter}>Submit</button>
             </form>
         </div>
     );
