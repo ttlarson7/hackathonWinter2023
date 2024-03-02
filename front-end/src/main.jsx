4a6bcc5120aa49eccaa727e836c852f31c12e5fb
Taz Larson <tazlarson1@gmail.com>
2024-03-01 19:54:32 -0800

    clerk set up

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
-
+import { ClerkProvider } from "@clerk/clerk-react";
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+ 
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-    
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <Router>
+        <App />
+      </Router>
+    </ClerkProvider>
   </React.StrictMode>,
 )
