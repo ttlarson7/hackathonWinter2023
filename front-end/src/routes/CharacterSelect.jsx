bd698dc82c72716d961c5370fffeeea6fe27ca42
ChiChan17 <114010974+ChiChan17@users.noreply.github.com>
2024-03-02 13:14:51 -0800

    CharacterSelect page and Cards

--- front-end/src/routes/CharacterSelect.jsx
+++ front-end/src/routes/CharacterSelect.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Dropdown from "./components/Dropdown";
-import Image from "./components/Image";
+import Image from "./components/Description";
+import Description from "./components/Description";
 
 export default function CharacterSelect() {
     
@@ -9,8 +10,8 @@ export default function CharacterSelect() {
             <div className="justify-left" id='dropdown'>
                 <Dropdown/>
             </div>
-            <div className="pl-100">
-                <Image/>
+            <div className="justify-right">
+                <Description/>
             </div>
         </div>
     );
