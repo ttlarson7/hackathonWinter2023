24211afec568618957df25cc5f990a145c987aac
Taz Larson <tazlarson1@gmail.com>
2024-03-03 12:54:31 -0800

    working on display

--- front-end/src/routes/components/Description.jsx
+++ front-end/src/routes/components/Description.jsx
@@ -10,10 +10,18 @@ import TieflingImage from "../../assets/tiefling.png";
 import HalflingImage from "../../assets/halfling.png";
 import DefaultImage from "../../assets/default.jpg";
 import { GlobalStateContext } from "../../App";
-
+import { useEffect } from "react";
 export default function Description() {
-  const { globalRace, globalClass, globalDescription } = React.useContext(GlobalStateContext);
-
+  const { globalRace, globalClass, globalDescription, globalBackground, globalAbilities, globalTraits, setGlobalTraits } = React.useContext(GlobalStateContext);
+  // speed: raceData['speed'],
+  //                                   age: raceData['age'],
+  //                                   size: raceData['size'],
+  //                                   sizeDescription: raceData['size_description'],
+  //                                   traits: traitNames,
+  //                                   traitsDescription: traitDescriptions,
+  useEffect(() => {
+    setGlobalTraits(globalAbilities[0]?.['traits'])
+  }, [globalAbilities[0]?.['traits']])
   const getImageSource = () => {
     switch (globalRace) {
       case "":
@@ -40,13 +48,29 @@ export default function Description() {
         return DefaultImage;
     }
   };
-
+  // newAbilities[1] =
+  //                               {
+  //                                   hitDie: classData['hit_die'],
+  //                                   proficiencies: proficiencies,
+  //                           savingThrows: savingThrows,
+  //                           levelDescriptions: levelDescriptions,
+  //                           startingEquipment: startingEquipment
+  //                       }
+// speed: raceData['speed'],
+  //                                   age: raceData['age'],
+  //                                   size: raceData['size'],
+  //                                   sizeDescription: raceData['size_description'],
+  //                                   traits: traitNames,
+  //                                   traitsDescription: traitDescriptions,
+  const test = () => {
+    console.log(globalAbilities[0]?.['traits'])
+  }
   return (
-    <div className="card flex w-96 shadow-xl">
-      <div className="bg-white max-w-xl rounded-lg overflow-hidden shadow-lg">
+    <div className="card flex w-4/5 shadow-xl">
+      <div className="bg-white w-full rounded-lg overflow-hidden shadow-lg">
         <div className="flex justify-center">
           <img
-            className="w-1/2"
+            className="w-1/3"
             src={getImageSource()}
             alt={globalRace}
           />
@@ -59,13 +83,48 @@ export default function Description() {
             Race: {globalRace}
           </div>
           <div className="uppercase tracking-wide text-sm font-semibold mt-4">
-            Description: 
+            Background: {globalBackground}
+          </div>
+          <div className="uppercase tracking-wide text-sm font-semibold mt-4">
+            Description:
           </div>
           <p className="text-lg">
             {globalDescription}
           </p>
+          <div className="flex justify-around">
+            <div className="flex flex-col max-w-1/2"> {/* Added max-w-1/2 class */}
+              <div className="uppercase tracking-wide text-sm font-semibold mt-4 self-center">
+                Race Abilities
+              </div>
+              <p className="text-lg max-w-80">
+                <b>Age:</b> {globalAbilities[0]?.['age']}
+              </p>
+              <p className="text-lg">
+                <b>Speed:</b> {globalAbilities[0]?.['speed']}
+              </p>
+              <p className="text-lg max-w-80">
+                <b>Size:</b> {globalAbilities[0]?.['sizeDescription']}
+              </p>
+              <p>{globalAbilities[0]?.['traits']}</p>
+            </div>
+            <div className="flex flex-col items-center max-w-1/2"> {/* Added max-w-1/2 class */}
+              <div className="uppercase tracking-wide text-sm font-semibold mt-4">
+                Class Abilities
+              </div>
+              <p className="text-lg max-w-80">
+                <b>Hit Dice</b> {globalAbilities[1]?.['hitDie']}
+              </p>
+              {globalAbilities[1]?.['proficiencies'].map((proficiency, index) => {
+                return <p key={index} className="text-lg"><b>Proficient In: </b>{proficiency}</p>
+              })}
+              {globalAbilities[1]?.['startingEquipment'].map((equipment, index) => {
+               return <p key={index} className="text-lg"><b>Starting Equipment: </b>{equipment}</p>
+              })}
+            </div>
+          </div>
         </div>
       </div>
+      <button onClick={test}>button</button>
     </div>
   );
 }
